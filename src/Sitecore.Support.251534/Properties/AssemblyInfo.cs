2eb345b10eaf1ba4b41f83bbd8e0750ec344163a
Ivan Sheyenko <ivs@sitecore.net>
2018-08-30 16:18:22 -0400

    Release generation semaphore in sendEmail pipelien for testMessageTask

---
+++ src/Sitecore.Support.251534/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.251534")]
+[assembly: AssemblyProduct("Sitecore.Support.251534")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.251534/ReleaseGenerationSemaphore.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.EmailCampaign.Cm.Dispatch;
+using Sitecore.EmailCampaign.Cm.Pipelines.SendEmail;
+using Sitecore.Modules.EmailCampaign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.EmailCampaign.Cm.Pipelines.SendEmail
+{
+  public class ReleaseGenerationSemaphore
+  {
+    public void Process(SendMessageArgs args)
+    {
+      if(args.Task is TestMessageTask)
+      {
+        EcmGlobals.GenerationSemaphore.ReleaseOne();
+      }
+    }
+  }
+}
